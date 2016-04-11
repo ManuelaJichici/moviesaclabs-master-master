9a8e68305d14708a4337d40e252a3fe00b2ef413
ManuelaJichici <buhuhu_mela@yahoo.com>
2016-04-11 16:54:47 +0300

    jquerry

---
+++ MoviesACLabs/Scripts/Movies.js
@@ -0,0 +1,21 @@
+﻿
+    var moviesSubmit = $("#movieSubmit");
+    var moviesNameInput = $("#Nume");
+    var moviesList = $("#moviesList");
+
+    moviesSubmit.click(function () {
+        var movieName = moviesNameInput.val();
+
+        var removeBtn = $("<button/>").html("x");
+        var newList1 = $("<li/>").html(movieName).hide();
+        newList1.append(removeBtn);
+       
+        moviesList.append(newList1);
+        newList1.slideDown();
+
+        removeBtn.click(function () {
+            newList1.remove();
+            $(this).remove();
+           })
+        console.log("ok");
+    })
