ea7d2cceb499b0bd8ab55a21d48d81d7d93c8e5e
ManuelaJichici <buhuhu_mela@yahoo.com>
2016-04-25 14:27:11 +0300

    angular

---
+++ MoviesACLabs/Scripts/AngularScript.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module('ang', []);
+
+
+app.controller('MainControler', function($scope){
+    $scope.addNewItem = "";
+    $scope.items = [ {Text:"item1", ID:1, Status:false} , {Text:"item2", ID:2 , Status : false} ];
+
+    var index = 3;
+
+    $scope.addItem = function(){
+        $scope.items.push({
+            Text : $scope.addNewItem,
+            ID: index++,
+            Status : false,
+        });
+     //   console.log($scope.items.pop())
+    };
+    $scope.countActive = items.length;
+  //  $scope.count = function () {
+
+    }
+
+    
+});
